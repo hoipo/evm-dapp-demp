2f2f8a6e691669f50e89b1345f908d2fa7c28858
potter.zhang <potter.zhang@bybit.com>
2025-02-28 16:45:33 +0800

    feat: Add development debugging tools and update configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
@@ -8,13 +9,23 @@ import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 import { config } from '../wagmi';
 
+
+
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // 仅在开发环境和客户端运行
+      import('eruda').then(module => {
+        const eruda = module.default;
+        eruda.init();
+      });
+  }, []);
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider coolMode>
+        <RainbowKitProvider>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import * as chains from 'viem/chains';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
-  // @ts-ignore
-  chains: Object.values(chains),
-  ssr: true,
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+  ],
+  ssr: false,
 });
